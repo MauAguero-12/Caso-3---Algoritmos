9e6dc1a611434e03acc65a2774310e783317e619
MauAguero-12 <mam.121002@gmail.com>
2021-03-03 19:45:57 -0600

    cambios cpp

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -7,12 +7,13 @@ bool findNumber(int pNumber, int pArray[], int pArraySize);
 void generateArray(int pArray[], int pSize);
 
 int main(){
+    int size = 100;
 
-    int array[100];
-    generateArray(array, 100);
+    int array[size];
+    generateArray(array, size);
 
     auto start = high_resolution_clock::now();
-    cout << "Resultado: " << findNumber(-1, array, 100) << endl;
+    cout << "Resultado: " << findNumber(-1, array, size) << endl;
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Tiempo: " << duration.count() << endl;
