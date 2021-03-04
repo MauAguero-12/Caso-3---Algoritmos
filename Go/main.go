1508fff89cada64ec21ae78970a305187ece393f
MauAguero-12 <mam.121002@gmail.com>
2021-03-03 19:46:30 -0600

    cambios go

--- Go/main.go
+++ Go/main.go
@@ -1,19 +1,36 @@
 package main
 
-import(
+import (
+	"math/rand"
 	"fmt"
+	"time"
 )
 
 func main(){
+	size := 100
 	array := []int{}
-	array = generateArray(array, 10)
-	fmt.Println(array)
-	fmt.Println("go funciona")
+	array = generateArray(array, size)
+	
+	start := time.Now()
+	fmt.Println(findNumber(-1, array, size))
+	elapsed := time.Since(start)
+	fmt.Println("Tiempo:", elapsed.Microseconds())
 }
 
 func generateArray(pArray []int, pSize int) []int{
+	seed := rand.NewSource(time.Now().UnixNano())
+	rand1 := rand.New(seed)
 	for index := 0; index < pSize; index++{
-		pArray = append(pArray, index)
+		pArray = append(pArray, rand1.Intn(100))
 	}
 	return pArray
+}
+
+func findNumber(pNumber int, pArray []int, pSize int) bool{
+	for index := 0; index < pSize; index++{
+		if pArray[index] == pNumber{
+			return true
+		}
+	}
+	return false
 }
